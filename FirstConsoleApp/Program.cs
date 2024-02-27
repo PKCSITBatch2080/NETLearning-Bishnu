08cc0a5830227e1186ad904c99b2eebfa55795d3
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-02-27 08:12:55 +0545

    Learn inheritance

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -18,12 +18,16 @@ Generics g = new();
 g.Display<string>("jsdlkfjsf ");
 g.Display<int>(786);
 
-Student iliya = new Student();
-Student smriti = new Student("Smiriti KC", 123);
+var iliya = new Student();
+var smriti = new Student("Smiriti KC", 123);
 
 iliya.RollNumber = 22;      // Assined
 smriti.RollNumber = 223;    // Not Assigned
 
+//smriti.Dob = DateTime.Now;
+var rekha = new CSStudent("Rekha Aryal", 24, "Hospital Managemenet System");
+rekha.DisplayInfo();
 
-
+var sapana = new CyberSecurityStudent("Sapana Shrestha", 11, "HMS", "RedHat");
+sapana.DisplayInfo();
 
