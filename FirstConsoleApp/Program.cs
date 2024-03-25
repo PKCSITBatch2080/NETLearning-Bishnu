306419bca1968acd5061feb661601ba0e493ae1a
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-03-25 07:24:55 +0545

    Learn Parallel and Async

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -9,20 +9,20 @@ Methods m = new();
 // m.Greet("Hi");
 // m.CalculateBMI(233.3f, 343.34f);
 
-var highest = m.GetMax(3.4, 5.6, 78.4, 23.4);
-var highest2 = m.GetMax(3.4, 5.6);
-var highest3 = m.GetMax(3.4, 5.6, 78.4, 23.4, 67.8);
-var highest4 = m.GetMax(3.4);
+// var highest = m.GetMax(3.4, 5.6, 78.4, 23.4);
+// var highest2 = m.GetMax(3.4, 5.6);
+// var highest3 = m.GetMax(3.4, 5.6, 78.4, 23.4, 67.8);
+// var highest4 = m.GetMax(3.4);
 
 Generics g = new();
 // g.Display<string>("jsdlkfjsf ");
 // g.Display<int>(786);
 
-var iliya = new Student();
-var smriti = new Student("Smiriti KC", 123);
+//var iliya = new Student();
+//var smriti = new Student("Smiriti KC", 123);
 
-iliya.RollNumber = 22;      // Assined
-smriti.RollNumber = 223;    // Not Assigned
+// iliya.RollNumber = 22;      // Assined
+// smriti.RollNumber = 223;    // Not Assigned
 
 // //smriti.Dob = DateTime.Now;
 // var rekha = new CSStudent("Rekha Aryal", 24, "Hospital Managemenet System");
@@ -33,4 +33,8 @@ smriti.RollNumber = 223;    // Not Assigned
 
 // CyberSecurityStudent.Do();
 
-FDHandling.CreateFile();
+//FDHandling.CreateFile();
+
+ParallelAndAsync pa = new();
+pa.ProcessNumbers();
+pa.ProcessNumbersParallel();
