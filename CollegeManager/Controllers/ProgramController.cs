0c63c2e8a68258da6ca153f399ea8de461b364e2
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-04-02 08:49:19 +0545

    Setup sqlite db

--- CollegeManager/Controllers/ProgramController.cs
+++ CollegeManager/Controllers/ProgramController.cs
@@ -2,18 +2,13 @@
 using Microsoft.AspNetCore.Mvc;
 public class ProgramController : Controller
 {
-    List<CollegeProgram> programs = 
-    [
-        new() { Id = 1, Name = "CSIT", Affliation = "TU", Started = DateTime.Now },
-        new() { Id = 2, Name = "BCA", Affliation = "TU", Started = DateTime.Now.AddYears(-2) },
-        new() { Id = 3, Name = "BIT", Affliation = "TU", Started = DateTime.Now.AddMonths(-11) },
-        new() { Id = 4, Name = "BIM", Affliation = "PU", Started = DateTime.Now.AddDays(-200) }
-    ];
-
     [HttpGet]
     public IActionResult Index()
     {
-        return View(programs);
+        CollegeManagerDb db = new();
+        var models = db.CollegePrograms.ToList();
+
+        return View(models);
     }
 
     [HttpGet]
@@ -26,7 +21,9 @@ public class ProgramController : Controller
     public IActionResult Add(CollegeProgram program) // model binding
     {
         // Do something on program
-        programs.Add(program);
+        CollegeManagerDb db = new();
+        db.CollegePrograms.Add(program);
+        db.SaveChanges();
         
         return RedirectToAction("Index");
     }
---
+++ CollegeManager/Data/CollegeManagerDb.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+public class CollegeManagerDb: DbContext
+{
+    public DbSet<CollegeProgram> CollegePrograms { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=CollegeManager.db");
+        // ConnectionStrings
+    }
+}
